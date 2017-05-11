f1381d22b96a2023a32895c9adf7adb18bfe2dbe
d.kotenko <d.kotenko@elgnc.com>
2017-05-11 08:56:26 +0300

    Added crud

--- DatabaseConnector/User.cs
+++ DatabaseConnector/User.cs
@@ -1,5 +1,6 @@
 ﻿namespace DatabaseConnector
 {
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
 
     public class User
@@ -13,36 +14,43 @@
         /// <summary>
         /// Імя
         /// </summary>
+        [DisplayName("Імя")]
         public string Name { get; set; }
 
         /// <summary>
-        /// Фамілія
+        /// Прізвище
         /// </summary>
+        [DisplayName("Прізвище")]
         public string Surname { get; set; }
 
         /// <summary>
         /// По батьові
         /// </summary>
+        [DisplayName("По батьові")]
         public string LastName { get; set; }
 
         /// <summary>
         /// e-mail 
         /// </summary>
+        [DisplayName("e-mail")]
         public string Email { get; set; }
 
         /// <summary>
         /// Телефон
         /// </summary>
+        [DisplayName("Назва")]
         public string Phone { get; set; }
 
         /// <summary>
         /// Місто
         /// </summary>
+        [DisplayName("Місто")]
         public string City { get; set; }
 
         /// <summary>
         /// Країна
         /// </summary>
+        [DisplayName("Країна")]
         public string Country { get; set; }
     }
 }
--- DatabaseLab/Controllers/HomeController.cs
+++ DatabaseLab/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 
 namespace DatabaseLab.Controllers
 {
+    using System.Data.Entity;
+    using System.Net;
     using DatabaseConnector;
 
     public class HomeController : Controller
@@ -24,6 +26,123 @@ namespace DatabaseLab.Controllers
             this.ViewBag.users = list;
 
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            try
+            {
+                if (this.ModelState.IsValid)
+                {
+                    this.dbContext.Users.Add(user);
+                    this.dbContext.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return this.View(user);
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = this.dbContext.Users.First(x => x.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = this.dbContext.Users.First(x => x.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            try
+            {
+                if (this.ModelState.IsValid)
+                {
+                    this.dbContext.Entry(user).State = EntityState.Modified;
+                    this.dbContext.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return this.View(user);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var user = this.dbContext.Users.First(x => x.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(User user)
+        {
+            try
+            {
+                if (this.ModelState.IsValid)
+                {
+                    this.dbContext.Entry(user).State = EntityState.Deleted;
+                    this.dbContext.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return this.View(user);
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
